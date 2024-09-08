a8c0404ad19b4b82bc615f1ba09d0e03498e4744
Oleh Shalashov <spirit14081992@gmail.com>
2024-09-08 16:14:00 -0300

    Update minor fixes

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -2,31 +2,38 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getMovieReviews } from "../../api";
 import css from "./MovieReviews.module.css";
+import { Loader } from "../Loader/Loader";
 
 export default function MovieReviews() {
 	const { movieId } = useParams();
 	const [reviews, setReviews] = useState([]);
+	const [isLoaded, setIsLoaded] = useState(false);
 
 	useEffect(() => {
 		const fetchReviews = async () => {
 			try {
+				setIsLoaded(false);
 				const data = await getMovieReviews(movieId);
 				setReviews(data);
 			} catch (error) {
 				console.error(error.message);
+			} finally {
+				setIsLoaded(true);
 			}
 		};
 
 		fetchReviews();
 	}, [movieId]);
 
+	if (!isLoaded) {
+		return <Loader />;
+	}
+
 	if (reviews.length === 0) {
 		return <p className={css.warning}>No reviews to show</p>;
 	}
 
-	return reviews.length === 0 ? (
-		<p className={css.warning}>No reviews to show</p>
-	) : (
+	return (
 		<div>
 			<h2 className={css.title}>Reviews</h2>
 			<ul className={css.reviewsList}>
